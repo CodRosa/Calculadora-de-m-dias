dda8f547928dd1018aed478710460fa82a2edab2
Rosa <95149319+CodRosa@users.noreply.github.com>
2022-09-06 16:47:30 -0300

    Add files via upload

---
+++ CalculadoradeMédia.js
@@ -0,0 +1,8 @@
+var NotadoPrimeiroBimestre = 10.0;
+var NotadoSegundoBimestre = 10.0;
+var NotadoTerceiroBimestre = 9.0;
+var NotadoQuartoBimestre = 9.0;
+
+var media = (NotadoPrimeiroBimestre + NotadoSegundoBimestre + NotadoTerceiroBimestre + NotadoQuartoBimestre )/ 4;
+var media = media.toFixed(1)
+console.log(media)
